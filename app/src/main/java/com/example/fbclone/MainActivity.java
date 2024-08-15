2c529c46e8148ff8f4c2e27580801d4bb38fc021
le chuck <hoanghl.22bi13170@usth.edu.vn>
2024-08-15 16:24:52 +0700

    added bot nav

--- app/src/main/java/com/example/fbclone/MainActivity.java
+++ app/src/main/java/com/example/fbclone/MainActivity.java
@@ -1,17 +1,17 @@
 package com.example.fbclone;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.fbclone.databinding.ActivityMainBinding;
 
+
 public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
     }
 }
