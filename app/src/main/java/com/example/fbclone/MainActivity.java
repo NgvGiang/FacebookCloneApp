0fafd7ba5d3ae12bb8146094567f44d6214659ec
Nguyen Van Giang <giangnv.22bi13132@usth.edu.vn>
2024-08-17 23:15:01 +0700

    add bottom nav with 5 layout

--- app/src/main/java/com/example/fbclone/MainActivity.java
+++ app/src/main/java/com/example/fbclone/MainActivity.java
@@ -2,16 +2,49 @@ package com.example.fbclone;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
 import com.example.fbclone.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    BottomNavigationView bottomNavigationView;
+    NavHostFragment navHostFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2);
+        NavController navController = navHostFragment.getNavController();
+
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if(itemId == R.id.navigation_home){
+                navController.navigate(R.id.navigation_home);
+            }else if (itemId == R.id.navigation_friends) {
+                navController.navigate(R.id.navigation_friends);
+            } else if (itemId == R.id.navigation_video) {
+                navController.navigate(R.id.navigation_video);
+            } else if (itemId == R.id.navigation_notification) {
+                navController.navigate(R.id.navigation_notification);
+            }else if (itemId == R.id.navigation_profile){
+                navController.navigate(R.id.navigation_profile);
+            }
+            return true;
+        });
+
+
 
     }
 }
